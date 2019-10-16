e9c9975538b397d6e0e14edc74286b36a18357c9
74ultra <r.barnhill@protonmail.com>
2019-10-16 16:00:06 -0700

    Updates

---
+++ precache-manifest.23cc4d0c494074bf62a4b428e7db3645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af86692b9774ef183f0d58ea902a8c6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9d0784cc29f6f31210ed",
+    "url": "/monsters-rolodex/static/css/main.cd0a8083.chunk.css"
+  },
+  {
+    "revision": "ab7103468dfa1378e2f9",
+    "url": "/monsters-rolodex/static/js/2.aed3a1aa.chunk.js"
+  },
+  {
+    "revision": "9d0784cc29f6f31210ed",
+    "url": "/monsters-rolodex/static/js/main.8e6d5889.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
